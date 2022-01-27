ebf582360470d41976a9c769c128fada0d1c0c36
JhoNBaratheoN <jhonatan.oliveira931002@gmail.com>
2022-01-27 20:53:35 -0300

    último commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import Api from "./Api";
+
 import ChatListItem from "./components/ChatListItem";
 import ChatIntro from "./components/ChatIntro";
+import ChatWindow from "./components/ChatWindow";
+import NewChat from "./components/NewChat";
+import Login from "./components/Login";
 
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
@@ -10,39 +15,56 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
 
 export default () => {
-  const [chatlist, setChatList] = useState([
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-  ]);
-  const [activeChate, setActiveChate] = useState({});
+  const [chatlist, setChatList] = useState([]);
+  const [activeChat, setActiveChat] = useState({});
+  const [user, setUser] = useState({
+    id: "TEmx7CUx6PsradnZdlpq",
+    name: "Jhonatan Oliveira",
+    avatar: "https://graph.facebook.com/4856166714496004/picture",
+  });
+  const [showNewChat, setShowNewChat] = useState(false);
+
+  useEffect(() => {
+    if (user !== null) {
+      let unsub = Api.onChatList(user.id, setChatList);
+      return () => unsub();
+    }
+  }, [user]);
+
+  const handleNewChat = () => {
+    setShowNewChat(true);
+  };
+
+  const handleLoginData = async (u) => {
+    let newUser = {
+      name: u.uid,
+      name: u.displayName,
+      avatar: u.photoURL,
+    };
+    await Api.addUser(newUser);
+    setUser(newUser);
+  };
+
+  if (user === null) {
+    return <Login onReceive={handleLoginData} />;
+  }
 
   return (
     <div className="app-window">
       <div className="sidebar">
+        <NewChat
+          chatlist={chatlist}
+          user={user}
+          show={showNewChat}
+          setShow={setShowNewChat}
+        />
         <header>
-          <img
-            className="header-avatar"
-            src="https://www.fernandesedutra.com.br/wp-content/uploads/2021/02/avatar.png"
-          />
+          <img className="header-avatar" src={user.avatar} alt="avatar" />
           <div className="header-buttons">
             <div className="header-btn">
               <DonutLargeIcon style={{ color: "#919191" }} />
             </div>
-            <div className="header-btn">
+            <div onClick={handleNewChat} className="header-btn">
               <ChatIcon style={{ color: "#919191" }} />
             </div>
             <div className="header-btn">
@@ -62,12 +84,22 @@ export default () => {
         </div>
         <div className="chatlist">
           {chatlist.map((item, key) => (
-            <ChatListItem key={key} />
+            <ChatListItem
+              key={key}
+              data={item}
+              active={activeChat.chatId === chatlist[key].chatId}
+              onClick={() => setActiveChat(chatlist[key])}
+            />
           ))}
         </div>
       </div>
       <div className="contentarea">
-        <ChatIntro />
+        {activeChat.chatId !== undefined && <ChatWindow
+        user={user}
+        data={activeChat} />
+        }
+
+        {activeChat.chatId === undefined && <ChatIntro />}
       </div>
     </div>
   );
--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ChatListItem.css";
 
-export default () => {
-    return (
-        <div className="chatListItem">
-            <img className="chatListItem-avatar" src="https://www.fernandesedutra.com.br/wp-content/uploads/2021/02/avatar.png" alt="" />
-            <div className="chatListItem-lines">
-                <div className="chatListItem-line">
-                    <div className="chatListItem-name">João Grilo</div>
-                    <div className="chatListItem-date">19:06</div>
-            </div>
-            <div className="chatListItem-line">
-                <div className="chatListItem-lastMsg">
-                    <p>Estava procurando um projeto similar a esse, porém tenho a ideia de usar autenticação por SMS que nem no WhatsApp. Usar o número do telefone e ao invés de contatos o app seria um chatbot com IA usando IBM Assistant</p>
-                </div>
-            </div>
-            </div>
+export default ({ onClick, active, data }) => {
+  
+  const [time, setTime] = useState("");
 
+  useEffect(() => {
+    if (data.lastMessageDate > 0) {
+      let d = new Date(data.lastMessageDate.seconds * 1000);
+      let hours = d.getHours();
+      let minutes = d.getMinutes();
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      setTime(`${hours}:${minutes}`);
+    }
+  }, [data]);
+
+  return (
+    <div className={`chatListItem ${active ? "active" : ""}`} onClick={onClick}>
+      <img className="chatListItem-avatar" src={data.image} alt="" />
+      <div className="chatListItem-lines">
+        <div className="chatListItem-line">
+          <div className="chatListItem-name">{data.title}</div>
+          <div className="chatListItem-date">{time}</div>
         </div>
-    );
+        <div className="chatListItem-line">
+          <div className="chatListItem-lastMsg">
+            <p>{data.lastMessage}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
